625ac73abaa5b61f1988bb7170a649a3d15855b9
akhilag7878 <62545456+akhilag7878@users.noreply.github.com>
2025-01-30 21:00:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //treats all JS code as newer version
+// alert(3+3) //we are using nodejs, not browswer
+
+let name = "akhil"
+let age = 28
+let isLoggedin = false
+
+//number range 2 to power 53
+//bigint
+//String : quotes single aur double(better)
+//boolean true/false
+//null =>standalone value
+//undefined 
+//symbol =>unique(mostly used in react for different containers)
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
